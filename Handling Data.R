5abeb894fd20ce31c41d88ecfd55d3979553bfd4
Ciera Price <you@example.com>
2025-02-25 12:12:43 -0600

    Complete randomized data table.

--- Handling Data.R
+++ Handling Data.R
@@ -45,7 +45,7 @@ Dat0 <- import(datafile0)
 Dat0[1,] #Take the 1st row of the data set
 Dat0[1,2] #Row 1, Col 2
 Dat0[1,2:5] #Row 1, Cols 2 through 5
-Dat0[1,c(2,5)] #Row 1, Cols 2 and 5 #C command is helpful for nonsequential numbers
+Dat0[1,c(2,5)] #Row 1, Cols 2 and 5 #C command is helpful for non-sequential data
 Dat0[c(1,1),]#Row 1 twice, All Cols
 Dat0[rep(1,6),] #Row 1, repeated 6 times, All Cols 
 #Rep command is helpful for repeating large amounts of data as the c command must be manually input
@@ -53,18 +53,31 @@ Dat0[rep(1,6),] #Row 1, repeated 6 times, All Cols
 set.seed(seed) #Use this func to replicate the same vector of random numbers; seed was defined on line 25
 rnorm(nrows, mean=900, sd=250) #Create a single column vector of random values; nrows was defined on line 25
 
-Dat1 <- Dat0[rep(1,nrows),];#Create a new variable from Dat ) with row 1 repeated nrows times for all columns
-mutate(Dat1, `CD4 ABS`=12) #Modify Dat 1 by replacing all values in column in CD4 ABS with '12'
-Dat1 <-mutate(Dat1, `CD4 ABS`=rnorm(n(), mean=900, sd=250)); #Redefine Dat1 by replacing the values in CD4 ABS with random values
+Dat1 <- Dat0[rep(1,nrows),];#Create a new variable from Dat 0 with row 1 repeated nrows times for all columns
+mutate(Dat1, `CD4 ABS`=12) #Modify Dat 1 by replacing all values in column CD4 ABS with '12'
+
+#Redefine Dat1 by replacing the values in specified columns with random values
 #n() represents the number of rows in the current block of data
 
 Dat1 <-mutate(Dat1
+              , across(is.numeric,~rnorm(n(), mean=.x, sd=1+.x/12)) 
+              #Across performs a specific action y given a condition x.
+              #In this case, if a value is numeric, then replace it with a random value.
               , ID= sprintf("EX-%04d",sample(1:1000,n())) # Zero pad to 4 places with prefix 'EX-'
+              ,`Specimen ID`= sprintf("%03d-%03d-%04d-%d",#`These quotes are needed for variable names that include a space`
+                                      sample(1:100,n(), replace = TRUE),
+                                      sample(1:100,n(),replace = TRUE),
+                                      sample(1:1000,n(),replace = TRUE),
+                                      sample(1:9,n(),replace = TRUE))
+              , PIN = seq_len(n())
               , `CD4 ABS`=round(rnorm(n(), mean=900, sd=250))
+              , `CD8 ABS`=round(rnorm(n(), mean=500, sd=20))
+              , `CD4/8 Ratio`=(`CD4 ABS`/`CD8 ABS`)
               , WBC=rnorm(n(), mean=4.9, sd=.26)
               , RBC=rnorm(n(), mean=8.7, sd=.24)
               )
 
-
-#sprintf("name=%s, age=%d, percentile=%% %f", "Ciera", 30, 98.5)
+#sprintf("name = %s, age = %d, percentile = %f %%", "Ciera", 30, 98.5)
 # s = string, d = integer, f = fraction
+
+
