d1b9f944d0209b07317eeeea07d34cd64a13c0e3
Ciera Price <you@example.com>
2025-03-25 12:01:41 -0500

    Created "Simulated Data" file

--- Handling Data.R
+++ Handling Data.R
@@ -36,6 +36,7 @@ library(dplyr); #add dplyr library
 
 options(max.print=500);
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+prob_missing=c(.99,.01)
 whatisthis <- function(xx){
   list(class=class(xx),info=c(mode=mode(xx),storage.mode=storage.mode(xx)
                               ,typeof=typeof(xx)))};
@@ -53,31 +54,42 @@ Dat0[rep(1,6),] #Row 1, repeated 6 times, All Cols
 set.seed(seed) #Use this func to replicate the same vector of random numbers; seed was defined on line 25
 rnorm(nrows, mean=900, sd=250) #Create a single column vector of random values; nrows was defined on line 25
 
-Dat1 <- Dat0[rep(1,nrows),];#Create a new variable from Dat 0 with row 1 repeated nrows times for all columns
-mutate(Dat1, `CD4 ABS`=12) #Modify Dat 1 by replacing all values in column CD4 ABS with '12'
-
+#Modify Data----
+#Create a new data frame from Dat 0 with row 1 repeated nrows times for all columns
 #Redefine Dat1 by replacing the values in specified columns with random values
-#n() represents the number of rows in the current block of data
+  #Across performs a specific action y given a condition x. 
+    #In this case, if a value is numeric, then replace it with a random value with the given mean and sd
+    #n() represents the number of rows in the current block of data
+    #.x is a placeholder for the column name - this syntax only works within mutate, transmute, and summarize
 
-Dat1 <-mutate(Dat1
-              , across(is.numeric,~rnorm(n(), mean=.x, sd=1+.x/12)) 
-              #Across performs a specific action y given a condition x.
-              #In this case, if a value is numeric, then replace it with a random value.
-              , ID= sprintf("EX-%04d",sample(1:1000,n())) # Zero pad to 4 places with prefix 'EX-'
-              ,`Specimen ID`= sprintf("%03d-%03d-%04d-%d",#`These quotes are needed for variable names that include a space`
-                                      sample(1:100,n(), replace = TRUE),
-                                      sample(1:100,n(),replace = TRUE),
-                                      sample(1:1000,n(),replace = TRUE),
-                                      sample(1:9,n(),replace = TRUE))
-              , PIN = seq_len(n())
-              , `CD4 ABS`=round(rnorm(n(), mean=900, sd=250))
-              , `CD8 ABS`=round(rnorm(n(), mean=500, sd=20))
-              , `CD4/8 Ratio`=(`CD4 ABS`/`CD8 ABS`)
-              , WBC=rnorm(n(), mean=4.9, sd=.26)
-              , RBC=rnorm(n(), mean=8.7, sd=.24)
-              )
+Dat1 <- Dat0[rep(1,nrows),] %>% 
+        mutate(across(where(is.numeric),~rnorm(n(), mean=.x, sd=1+.x/12))
+        , ID= sprintf("EX-%04d",sample(1:1000,n())) # Zero pad to 4 places with prefix 'EX-'
+        ,`Specimen ID`= sprintf("%03d-%03d-%04d-%d"#`These quotes are needed for variable names that include a space`
+              ,sample(1:100,n(), replace = TRUE)
+              ,sample(1:100,n(),replace = TRUE)
+              ,sample(1:1000,n(),replace = TRUE)
+              ,sample(1:9,n(),replace = TRUE))
+        , PIN = seq_len(n())
+        , `CD4 ABS`=round(rnorm(n(), mean=900, sd=250))
+        , `CD8 ABS`=round(rnorm(n(), mean=500, sd=20))
+        , `CD4/8 Ratio`=(`CD4 ABS`/`CD8 ABS`)
+        , WBC=rnorm(n(), mean=4.9, sd=.26)
+        , RBC=rnorm(n(), mean=8.7, sd=.24)
+        , across(everything(),~ifelse(sample(1:0,n(),replace=TRUE,prob = prob_missing),.x, NA))
+        , Date=as.Date(Date,"%m/%d/%Y")-sample(0:2,n(),replace=TRUE) 
+        )
 
 #sprintf("name = %s, age = %d, percentile = %f %%", "Ciera", 30, 98.5)
 # s = string, d = integer, f = fraction
 
+#Generate a Date column
+#as.Date("11/30/2024","%m/%d/%Y") #Used to define format of date
+#class(as.Date("11/30/2024","%m/%d/%Y")) #What class of data is this? May use function to confirm that you've created a date.
+#is(as.Date("11/30/2024","%m/%d/%Y"),"Date") ## Alternatively, you can ask is argument X class Y? Returns TRUE or FALSE.
+#as.Date("11/30/2024","%m/%d/%Y")-sample(0:2,size=3)
 
+SummaryDat1 <-summarize(group_by(Dat1,Date),`CD4 ABS`=mean(`CD4 ABS`),`CD8 ABS`=mean(`CD8 ABS`))
+
+#Write Data ----
+export(Dat1,"Data/Simulated Data.xlsx")
